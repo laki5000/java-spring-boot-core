91806d8951ef1e7386eb24c9e74eb849be068a59
Lakatos Bence <lakatos816@gmail.com>
2026-09-25 18:03:55 +0200

    spotless

--- backend/src/main/java/com/example/core/message/I18nService.java
+++ backend/src/main/java/com/example/core/message/I18nService.java
@@ -3,7 +3,6 @@ package com.example.core.message;
 import com.example.core.logging.LogExecution;
 import java.util.Locale;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.event.Level;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
